8ba52a3aa8c6223abbca12a0346dad58eb7b5a19
Rafael Moraes <contact@moraesrafael.com>
2015-11-30 19:42:58 -0500

    fix port listen

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
